11599d8ae51ef7a86312d2da9d4a42060044c8e9
BruceHaley <brucehaleyemail@gmail.com>
2023-06-18 18:34:31 -0700

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -19,10 +19,10 @@ cd ${DIR};
 for file in *.* ;
 do
   echo "file=" "/${DIR}/${file}";
-  echo "log -1=" $(git log --full-history -- "/${DIR}/${file}")
+  echo "log -1=" $(git log --full-history -- "${DIR}/${file}")
 
   # Get the timestamp of the last commit that modified this file
-  LAST_MODIFIED_TIMESTAMP=$(git log -1 --format="%at" -- "./${DIR}/${file}")
+  LAST_MODIFIED_TIMESTAMP=$(git log -1 --format="%at" -- "${DIR}/${file}")
   echo "LAST_MODIFIED_TIMESTAMP=" $LAST_MODIFIED_TIMESTAMP
 
   if [ "$LAST_MODIFIED_TIMESTAMP" -lt "$X_DAYS_AGO_TIMESTAMP" ]
